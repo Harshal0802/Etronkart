45af873edaf32b405aa774e4fdde274505c22308
harshal <harshrathod1111@gmail.com>
2023-11-29 23:23:15 +0530

    created header and footer

---
+++ app/components/nav/Navbar.tsx
@@ -0,0 +1,24 @@
+import Container from "../Container";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <div className="sticky top-0 shadow-sm w-full bg-slate-200 z-30">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md:gap-0">
+            <Link href="/" className="font-bold text-xl">
+              EtronKart
+            </Link>
+            <div className="hidden md:block">Search</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <div>Cart Count</div>
+              <div>User Menu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
