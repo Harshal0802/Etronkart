07d4867cf5ca7a51f6442a6f571fdf9006ab7fe4
harshal <harshrathod1111@gmail.com>
2023-11-30 13:36:54 +0530

    created product cards in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
+import ProductCard from "./components/products/ProductCard";
 import { products } from "@/utils/products";
-import { truncateText } from "@/utils/truncateText";
+
 export default function Home() {
   return (
     <div className="p-8">
@@ -11,7 +12,11 @@ export default function Home() {
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {products.map((product: any) => {
-            return <div>{truncateText(product.name)}</div>;
+            return (
+              <div key={product.key}>
+                <ProductCard data={product} />
+              </div>
+            );
           })}
         </div>
       </Container>
